bda1d2d245dcf464f81a6e70edd94fc84be51b7c
MrBearTW_NKNU <jamieperng@gmail.com>
2018-09-28 17:19:57 +0800

    20180928NKNU

---
+++ project/opencart_orignal/upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/admin/');
+define('DIR_SYSTEM', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/system/');
+define('DIR_IMAGE', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/image/');
+define('DIR_LANGUAGE', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/admin/language/');
+define('DIR_TEMPLATE', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/admin/view/template/');
+define('DIR_CONFIG', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/system/config/');
+define('DIR_CACHE', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/system/storage/download/');
+define('DIR_LOGS', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/system/storage/upload/');
+define('DIR_CATALOG', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ project/opencart_orignal/upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/catalog/');
+define('DIR_SYSTEM', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/system/');
+define('DIR_IMAGE', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/image/');
+define('DIR_LANGUAGE', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/catalog/language/');
+define('DIR_TEMPLATE', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/system/config/');
+define('DIR_CACHE', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/system/storage/download/');
+define('DIR_LOGS', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/Users/APP User/Documents/github/nknuapp/project/opencart_orignal/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
